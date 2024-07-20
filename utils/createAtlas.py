3a531d70c18eb6ed92f1bf66e7be22ff8e6faff2
Luca <lucchiwucchi@gmail.com>
2024-07-20 23:33:35 +1200

    New testing textures
    New texture atlas with six basic textures

--- utils/createAtlas.py
+++ utils/createAtlas.py
@@ -1,10 +1,10 @@
 from PIL import Image
 
 def create_texture_atlas(textures, output_path):
-    atlas_size = (1024, 1024)
+    atlas_size = (1024, 1536)
     atlas = Image.new('RGB', atlas_size)
     
-    positions = [(0,0), (512,0)]
+    positions = [(0,0), (512,0), (0,512), (512,512),(0,1024),(512,1024)]
     
     for texture_path, position in zip(textures, positions):
         texture = Image.open(texture_path)
@@ -12,5 +12,5 @@ def create_texture_atlas(textures, output_path):
     
     atlas.save(output_path, 'BMP')
 
-textures = ['texture1.png', 'texture2.png']
+textures = ['Sprite-0001.png', 'Sprite-0002.png', 'Sprite-0003.png','Sprite-0004.png','Sprite-0005.png','Sprite-0006.png']
 create_texture_atlas(textures, '../texture_atlas.bmp')
